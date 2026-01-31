63acfbb88f25b17652bfaef92cb0ff64deefd407
sukanto01899 <sukanto01899@gmail.com>
2026-01-31 17:17:14 +0600

    signer added

--- contracts/Multisign.sol
+++ contracts/Multisign.sol
@@ -3,39 +3,59 @@ pragma solidity ^0.8.24;
 
 
 contract MultiSign {
-
-    struct Account {
-        uint256 accountId;
-        address AccountCreator;
-        mapping(address => bool) verifiedOwners;
-        uint256 balance;
-        address[] bindAddress;
-    }
-    struct Sign {
-        address[] Signers;
-        mapping(address => bool) addressSigned;
-
-    }
-
+    address[] owners;
+    mapping(address => bool) realSigner;
+    address owner;
     uint256 nextId;
-    mapping(uint256 => Account) accounts;
 
     constructor (){
-        nextId = 1;
+        owner = msg.sender;
     }
 
-    function createMultiSignAccount() external {
-        Account storage NewAccount = accounts[nextId];
-        NewAccount.accountId = nextId;
-        NewAccount.AccountCreator = msg.sender;
-        NewAccount.balance = 0;
-        NewAccount.bindAddress = new address[](1);
-        NewAccount.bindAddress[0] = msg.sender;
-        NewAccount.verifiedOwners[msg.sender] = true;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not owner");
+        _;
+    }
+
+    function addSigner(address _signer) external onlyOwner() {
+        require(!realSigner[_signer], "Signer already added!");
+
+        owners.push(_signer);
+        realSigner[_signer] = true;
+    }
+
+   
+
+    struct Withdraw{
+        uint256 id;
+        bool isCompleted;
+        uint256 amount;
+        mapping(address => bool) signedBy;
+        address createdBy;
+        address to;
+    }
+    mapping(uint256 => Withdraw) withdrawals;
+
+    function InitWithdraw(uint256 _amount, address _to) external{
+        require(realSigner[msg.sender], "You are not signer!");
+        require(address(this).balance >= _amount, "Insufficient balance");
+
+        withdrawals[nextId].id = nextId;
+        withdrawals[nextId].isCompleted = false;
+        withdrawals[nextId].amount = _amount;
+        withdrawals[nextId].createdBy = msg.sender;
+        withdrawals[nextId].to = _to;
+
         nextId++;
     }
 
-    function addSignerToAccount() external{
-        
+
+    function addSign(uint256 _withdrawId) public{
+
+        Withdraw storage getWithdrawals = withdrawals[_withdrawId];
+        address sign = msg.sender;
+
+        require(realSigner[sign], "Not valid signer");
+        require(!getWithdrawals.signedBy[sign], "Already signed");
     }
 }
